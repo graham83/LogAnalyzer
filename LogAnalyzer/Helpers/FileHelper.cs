ab467bc0b5faf131ffccb68fe3b5a08f39cb8a4f
Graham-ABRI <graham@thesoundtrader.com>
2022-02-22 10:52:00 +1100

    Added file helper and initial line parser with unit test.

---
+++ LogAnalyzer/Helpers/FileHelper.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogAnalyzer.Data;
+using LogAnalyzer.Models;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace LogAnalyzer.Business
+{
+    public class FileHelper
+    {
+        private readonly string[] HTTP_ACTIONS = new string[] { "GET", "POST", "PUT", "DELETE" };
+        private readonly int MAX_NO_LOG_ITEMS = 9;
+        private readonly int MIN_NO_LOG_ITEMS = 5;
+
+        private ILogRepository _logRepository;
+   
+
+        public List<string> BadLines { get; set; } = new List<string>();
+
+        public FileHelper(ILogRepository repository)
+        {
+            _logRepository = repository;
+        }
+
+        public int GetNumberUniqueUrls()
+        {
+            return _logRepository.GetNumberUniqueIpAddresses();
+        }
+
+        public List<string> GetTop3Visited()
+        {
+            return _logRepository.MostVisitedUrls(3);
+        }
+
+        public List<string> GetTop3ActiveIps()
+        {
+            return _logRepository.MostActiveIpAddresses(3);
+        }
+
+        public bool CheckFileExists(string path)
+        {
+            if (File.Exists(path))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckFileLineFormatOk(string fileLine)
+        {
+
+            var lineValues = GetLineComponents(fileLine);
+
+            if (!ReadLineSanityCheck(lineValues)) return false;
+
+            LogRecord ? logRecord = ParseLineRaw(lineValues);
+
+            if (logRecord != null)
+            {
+                return true;
+            }
+            
+            return false;
+        }
+
+
+        private static List<string> GetLineComponents(string line)
+        {
+            int endStringIndex = 0, startIndex = 0;
+
+            List<string> components = new List<string>();
+
+            while (endStringIndex != -1)
+            {
+           
+                if (line[startIndex] == '\"')     // Capture quoted inline strings
+                {
+                    endStringIndex = line.IndexOf('\"', startIndex + 1) + 1;
+                }
+                else if (line[startIndex] == '[') // Capture datetime included in brackets
+                {
+                    endStringIndex = line.IndexOf(']', startIndex + 1) + 1;
+                }
+                else  // Otherwise get all fields between the spaces
+                {
+                    endStringIndex = line.IndexOf(' ', startIndex + 1) == -1 ? line.Length - 1 :
+                        line.IndexOf(' ', startIndex + 1);
+                }
+
+                components.Add(line.Substring(startIndex, endStringIndex - startIndex));
+
+                startIndex = line.IndexOf(' ', endStringIndex);
+
+                if (startIndex == -1) break;
+
+                startIndex++;
+
+            }
+
+            return components;
+        }
+
+        private bool ReadLineSanityCheck(List<string> record)
+        {
+            // check length
+            if (record.Count > MAX_NO_LOG_ITEMS ||
+                record.Count < MIN_NO_LOG_ITEMS) return false;
+
+            // check URL has real Action
+            if (Array.IndexOf(HTTP_ACTIONS, GetUrlActionFromString(record[4])) == -1) return false; 
+
+            // check URL path is legit
+            if (Uri.IsWellFormedUriString(GetUrlPathFromString(record[4]), UriKind.Absolute) == false &&
+                (Uri.IsWellFormedUriString(GetUrlPathFromString(record[4]), UriKind.Relative) == false)) return false;
+
+            // check DateTime field expected format
+            if (record[3].Contains('[') == false ||
+                record[3].Contains(']') == false) return false;
+
+            
+            return true;
+        }
+
+        private static string GetUrlPathFromString(string component)
+        {
+            return component.Split(' ')[1].Replace("\"", string.Empty);
+        }
+
+        private static string GetUrlActionFromString(string component)
+        {
+            return component.Split(' ')[0].Replace("\"", string.Empty);
+        }
+        private LogRecord? ParseLineRaw(List<string> components)
+        {
+
+            try
+            {
+                return new LogRecord
+                {
+                    SourceIpAddress = components[0],
+                    LastUpdated = components[3],
+                    HttpAction = GetUrlActionFromString(components[4]),
+                    Url = GetUrlPathFromString(components[4]),
+                    HttpResponse = int.Parse(components[5]),
+                    ContentSize = int.Parse(components[6]),
+                    Info = components[8]
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+            return null;
+        }
+
+    }
+}
---
+++ LogAnalyzerTests/Helpers/FileHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogAnalyzer.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using LogAnalyzer.Data;
+using LogAnalyzer.Models;
+
+namespace LogAnalyzer.Helpers.Tests
+{
+    [TestClass()]
+    public class FileHelperTests
+    {
+        string HEALTHY_LINE = "177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] \"GET /intranet-analytics/ HTTP/1.1\" 200 3574 \"-\" \"Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7\"";
+       
+
+        [TestMethod()]
+        public void CheckFileLineFormatOkTestGoodDataReturnsTrue()
+        {
+            // Arrange
+            var repositoryMock = new Mock<ILogRepository>();
+
+            var helper = new FileHelper(repositoryMock.Object);
+
+            //Act
+            var goodLine = helper.CheckFileLineFormatOk(HEALTHY_LINE);
+
+            //Assert
+
+            Assert.AreEqual(true, goodLine);
+        }
+    }
+}
