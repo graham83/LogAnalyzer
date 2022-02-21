827b1313d39e9e69f583a515c88c52e8096ca41f
Graham-ABRI <graham@thesoundtrader.com>
2022-02-22 09:23:46 +1100

    Add repository interface

---
+++ LogAnalyzer/Data/ILogRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogAnalyzer.Models;
+
+namespace LogAnalyzer.Data
+{
+    public interface ILogRepository : IDisposable
+    {
+        IEnumerable<LogRecord> GetLogRecords();
+        int GetNumberUniqueIpAddresses();
+        List<string> MostVisitedUrls(int number);
+        List<string> MostActiveIpAddresses(int number);
+
+
+    }
+}
