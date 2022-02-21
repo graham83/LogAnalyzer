8f30f521557b8a0fe40546574e9746fadc807257
Graham-ABRI <graham@thesoundtrader.com>
2022-02-22 09:21:33 +1100

    Add DB context and basic Log Model

---
+++ LogAnalyzer/Data/LogDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LogAnalyzer.Models;
+
+namespace LogAnalyzer.Data
+{
+    public class LogDbContext : DbContext
+    {
+        public DbSet<LogRecord> LogRecords { get; set; }
+
+        public LogDbContext(DbContextOptions<LogDbContext> options) : base(options)
+        {
+
+        }
+ 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LogRecord>().ToTable("LogRecords");
+        }
+    }
+}
---
+++ LogAnalyzer/Models/LogRecord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAnalyzer.Models
+{
+    public class LogRecord
+    {
+        public int ID { get; set; }
+        public string SourceIpAddress { get; set; } = String.Empty;
+        public string LastUpdated { get; set; } = String.Empty;
+        public string HttpAction { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public int HttpResponse { get; set; }
+        public int ContentSize { get; set; }
+        public string Info { get; set; } = string.Empty;
+    }
+}
