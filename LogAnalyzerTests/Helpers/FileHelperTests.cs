2908afe82af21de943a34e6ad0ea6d15ecc9e8ae
Graham-ABRI <graham@thesoundtrader.com>
2022-02-22 11:04:49 +1100

    Additional unit tests for bad or unexpected line data

--- LogAnalyzerTests/Helpers/FileHelperTests.cs
+++ LogAnalyzerTests/Helpers/FileHelperTests.cs
@@ -15,7 +15,12 @@ namespace LogAnalyzer.Helpers.Tests
     public class FileHelperTests
     {
         string HEALTHY_LINE = "177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] \"GET /intranet-analytics/ HTTP/1.1\" 200 3574 \"-\" \"Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7\"";
-       
+
+        string LINE_WITH_BAD_ACTION = "177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] \"JUNK /intranet-analytics/ HTTP/1.1\" 200 3574 \"-\" \"Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7\"";
+        string LINE_WITH_UNEPECTED_DATE = "177.71.128.21 - - 10/Jul/2018:22:21:28 +0200] \"GET /intranet-analytics/ HTTP/1.1\" 200 3574 \"-\" \"Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7\"";
+        string LINE_WITH_TOO_MANY_FIELDS = "177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] \"GET /intranet-analytics/ HTTP/1.1\" 200 3574 \"-\" \"Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7\" junk field";
+        string LINE_WITH_TOO_FEW_FIELDS = "177.71.128.21 - - [10/Jul/2018:22:21:28 +0200]";
+        string LINE_WITH_BAD_URL = "177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] \"GET <> HTTP/1.1\" 200 3574 \"-\" \"Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7\"";
 
         [TestMethod()]
         public void CheckFileLineFormatOkTestGoodDataReturnsTrue()
@@ -31,6 +36,87 @@ namespace LogAnalyzer.Helpers.Tests
             //Assert
 
             Assert.AreEqual(true, goodLine);
+        }
+
+        [TestMethod()]
+        public void CheckFileLineFormatOkTestBadActionReturnsFalse()
+        {
+            // Arrange
+            var repositoryMock = new Mock<ILogRepository>();
+
+            var helper = new FileHelper(repositoryMock.Object);
+
+            //Act
+            var badLine = helper.CheckFileLineFormatOk(LINE_WITH_BAD_ACTION);
+
+            //Assert
+
+            Assert.AreEqual(false, badLine);
+        }
+
+        [TestMethod()]
+        public void CheckFileLineFormatOkTestBadDateReturnsFalse()
+        {
+            // Arrange
+            var repositoryMock = new Mock<ILogRepository>();
+
+            var helper = new FileHelper(repositoryMock.Object);
+
+            //Act
+            var badLine = helper.CheckFileLineFormatOk(LINE_WITH_UNEPECTED_DATE);
+
+            //Assert
+
+            Assert.AreEqual(false, badLine);
+        }
+
+
+        [TestMethod()]
+        public void CheckFileLineFormatOkTestTooManyFieldsReturnsFalse()
+        {
+            // Arrange
+            var repositoryMock = new Mock<ILogRepository>();
+
+            var helper = new FileHelper(repositoryMock.Object);
+
+            //Act
+            var badLine = helper.CheckFileLineFormatOk(LINE_WITH_TOO_MANY_FIELDS);
+
+            //Assert
+
+            Assert.AreEqual(false, badLine);
+        }
+
+
+        [TestMethod()]
+        public void CheckFileLineFormatOkTestTooFewFieldsReturnsFalse()
+        {
+            // Arrange
+            var repositoryMock = new Mock<ILogRepository>();
+
+            var helper = new FileHelper(repositoryMock.Object);
+
+            //Act
+            var badLine = helper.CheckFileLineFormatOk(LINE_WITH_TOO_FEW_FIELDS);
+
+            //Assert
+
+            Assert.AreEqual(false, badLine);
+        }
+        [TestMethod()]
+        public void CheckFileLineFormatOkTestBadUrlReturnsFalse()
+        {
+            // Arrange
+            var repositoryMock = new Mock<ILogRepository>();
+
+            var helper = new FileHelper(repositoryMock.Object);
+
+            //Act
+            var badLine = helper.CheckFileLineFormatOk(LINE_WITH_BAD_URL);
+
+            //Assert
+
+            Assert.AreEqual(false, badLine);
         }
     }
 }
